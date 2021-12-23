dda3129a816a958ae93f5cce0bd6c42d2953abd9
Ali Shatri <ali@Alis-MacBook-Pro.local>
2021-12-23 23:48:45 +0100

    JAVA projekt

---
+++ DSH.java
@@ -0,0 +1,20 @@
+/**
+ * DSH
+ */
+public class DSH {
+
+	public static void main(String[] args) {
+			int numri = 1 ;
+		  double numri2 = 1.1 ;
+	    System.out.println ("Plusi eshte " + (numri + numri2));
+			System.out.println("Minusi eshte " +( numri - numri2));
+			System.out.println("Shumzimi eshte " + (numri * numri2)) ;
+			System.out.println(" Pjestimi eshte " + (numri / numri2)) ;
+
+
+
+	}
+
+
+
+}
